4d58c4158e507764003d2c8d309b92e754dc729c
Tom Mitchell <tom@tom.net.nz>
2021-05-09 23:34:49 -0700

    Add list call

--- emoji_import.py
+++ emoji_import.py
@@ -27,14 +27,24 @@ def import_emoji(slack_token, rc_url, rc_user, rc_pass):
     rc_token, rc_userid = auth_rc(rc_url, rc_user, rc_pass)
     upload_count = 0
 
+    existing_emoji = list_rc_emoji(rc_url, rc_token, rc_userid)
+
     for macro, item in slack_emoji.items():
         if not item.startswith("https://"):
             continue
 
-        if create_rc_emoji(rc_url, rc_token, rc_userid, macro, item):
-            upload_count += 1
+        if macro in existing_emoji:
+            if update_rc_emoji(
+                rc_url, rc_token, rc_userid, existing_emoji[macro]["id"], macro, item
+            ):
+                upload_count += 1
+            else:
+                print("Failed to update emoji {}".format(macro))
         else:
-            print("Failed to create emoji {}".format(macro))
+            if create_rc_emoji(rc_url, rc_token, rc_userid, macro, item):
+                upload_count += 1
+            else:
+                print("Failed to create emoji {}".format(macro))
 
     print("Created {:d} emoji in Rocket.Chat".format(upload_count))
 
@@ -58,6 +68,24 @@ def auth_rc(rc_url, rc_user, rc_pass):
     return data["data"]["authToken"], data["data"]["userId"]
 
 
+def list_rc_emoji(rc_url, rc_token, rc_userid):
+    req = requests.post(
+        "{}/api/v1/emoji-custom.list".format(rc_url),
+        headers={
+            "X-Auth-Token": rc_token,
+            "X-User-Id": rc_userid,
+        },
+    )
+    req.raise_for_status()
+    return {
+        emoji["name"]: {
+            "id": emoji["_id"],
+            "aliases": emoji["aliases"],
+        }
+        for emoji in req.json()
+    }
+
+
 def create_rc_emoji(rc_url, rc_token, rc_userid, macro, url):
     image_data = requests.get(url, stream=True)
     print("Attempting to create emoji :{}: from URL: {}".format(macro, url))
@@ -79,7 +107,60 @@ def create_rc_emoji(rc_url, rc_token, rc_userid, macro, url):
         )
 
         if req.status_code == 400:
-            print("Invalid request - likely due to emoji already existing")
+            print(
+                "Invalid request - likely due to emoji already existing. Updating instead."
+            )
+            req = requests.post(
+                "{}/api/v1/emoji-custom.create".format(rc_url),
+                headers={
+                    "X-Auth-Token": rc_token,
+                    "X-User-Id": rc_userid,
+                },
+                data={"name": macro},
+                files={"emoji": fd},
+            )
+            return False
+
+        req.raise_for_status()
+
+        resp = req.json()
+        print(resp)
+        return resp["success"]
+
+
+def update_rc_emoji(rc_url, rc_token, rc_userid, id, macro, url):
+    image_data = requests.get(url, stream=True)
+    print("Attempting to update emoji :{}: from URL: {}".format(macro, url))
+
+    with tempfile.TemporaryFile() as fd:
+        for chunk in image_data.iter_content(chunk_size=128):
+            fd.write(chunk)
+
+        fd.seek(0)
+
+        req = requests.post(
+            "{}/api/v1/emoji-custom.update".format(rc_url),
+            headers={
+                "X-Auth-Token": rc_token,
+                "X-User-Id": rc_userid,
+            },
+            data={"name": macro, "_id": id},
+            files={"emoji": fd},
+        )
+
+        if req.status_code == 400:
+            print(
+                "Invalid request - likely due to emoji already existing. Updating instead."
+            )
+            req = requests.post(
+                "{}/api/v1/emoji-custom.create".format(rc_url),
+                headers={
+                    "X-Auth-Token": rc_token,
+                    "X-User-Id": rc_userid,
+                },
+                data={"name": macro},
+                files={"emoji": fd},
+            )
             return False
 
         req.raise_for_status()
